4768b45cf3225293d96f4db1fb040d0a377c923b
Joey Dreijer <joey.dreijer@ing.nl>
2019-07-13 13:19:28 +0200

    Added distinct screenshot filter

--- app/api_snapshots.py
+++ app/api_snapshots.py
@@ -1,6 +1,6 @@
 from app import app, api
 from flask import Flask, request, g, make_response
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, inputs
 from app.models import Snapshots
 from flask_jwt_extended import jwt_required
 
@@ -30,11 +30,22 @@ class APISnapshots(Resource):
         if request.method == "GET":
             self.args.add_argument('max', location='args', required=False, help='max entries', type=int, default=25)
             self.args.add_argument('search', location='args', required=False, default="")
+            self.args.add_argument('distinct', location='args', required=False, default=False, type=inputs.boolean)
 
     def get(self):
         args = self.args.parse_args()
-        get_snaps = Snapshots.objects(url__contains=args.search).order_by('-timestamp').limit(args.max)
-        results = json.loads(get_snaps.to_json())
+        if args.distinct:
+            pipeline = [ { "$group" : { "_id" : {"hash": "$sha256", "url": "$url" },
+                "screenshot": { "$first": "$screenshot"  },
+                "firstSeen": { "$first": "$timestamp" } } }, 
+                { "$limit": args.max } ]
+
+            get_snaps = Snapshots.objects(url__contains=args.search).aggregate(*pipeline)
+            results = json.loads(bson.json_util.dumps(get_snaps))
+        else:
+            get_snaps = Snapshots.objects(url__contains=args.search).order_by('-timestamp').limit(args.max)
+            results = json.loads(get_snaps.to_json())
+
         return results
 
 api.add_resource(APISnapshots, '/api/v1/snapshots')
